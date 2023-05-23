4ca65695c9774d6ea6b975dacf43c87f23af979f
gsbassanezi <gsbassanezi@gmail.com>
2023-05-23 00:11:01 -0300

    create form in modal

--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -8,25 +8,21 @@
     {{-- Form Modal --}}
     <x-dialog-modal wire:model="modalFormVisible">
         <x-slot name="title">
-            {{ __('Delete Account') }}
+            <x-label for="title" value="{{ __('Title') }}" />
+            <x-input id="title" type="text" class="mt-1 block w-full" wire:model.debounce.800ms="title"
+                autofocus />
+            <x-input-error for="title" class="mt-2" /><br>
         </x-slot>
 
         <x-slot name="content">
-            <x-label for="title" value="{{ __('Title') }}" />
-            <x-input id="title" type="text" class="mt-1 block w-full" wire:model.defer="createApiTokenForm.title"
-                autofocus />
-            <x-input-error for="title" class="mt-2" /><br>
-
-                <div class="btx">
-                    <x-label for="slug" value="{{ __('Slug') }}" />
-                    <div class="relative mb-4 flex flex-wrap items-stretch w-full">
-                        <span
-                            class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
-                            >https://localhost/</span>
-                        <input type="text" name="slug" id="slug" autocomplete="slug" class="rounded dark:bg-gray-800" placeholder="slug here" />
-                        <x-input-error for="slug" class="mt-2" />
-                    </div>
-
+            <x-label for="slug" value="{{ __('Slug') }}" />
+            <div class="relative mb-4 flex flex-wrap items-stretch w-full">
+                <span
+                    class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+                    >https://localhost/</span>
+                <x-input type="text" id="slug" wire:model="slug" class="rounded dark:bg-gray-800" placeholder="slug here" />
+                <x-input-error for="slug" class="mt-2" />
+            </div>
 
                 <x-label for="content" value="{{ __('Content') }}" />
                 <x-input id="content" type="text" class="mt-1 block w-full"
